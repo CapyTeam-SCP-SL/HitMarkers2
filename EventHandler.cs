0f94d6db3e54b018d6cd3bd20239154f826b2610
TranquilityStudio <egoriesgames.gamestudio@gmail.com>
2023-09-22 22:24:28 +0500

    Some FriendlyFire Fixes
    Some FriendlyFire Fixes

---
+++ EventHandler.cs
@@ -0,0 +1,53 @@
+﻿using Exiled.Events.EventArgs.Player;
+using Exiled.API.Features;
+using PlayerRoles;
+using System;
+using System.Diagnostics;
+
+namespace HitMarkers2
+{
+    public class EventHandler
+    {
+        Config _config = HitMarkers2.Singleton.Config;
+
+        public void HurtingEvent(HurtingEventArgs ev)
+        {
+
+            if (ev.Attacker == null || ev.Player == null)
+                return;
+            if (!ev.IsAllowed || ev.Amount <= 0.01f || ev.Attacker == ev.Player)
+                return;
+
+            if (ev.Attacker.Role.Team == Team.SCPs && _config.EnableScpHints == false)
+                return;
+
+            if (!Server.FriendlyFire && ev.Attacker.Role.Side == ev.Player.Role.Side)
+                return;
+
+            string attackerHintBuilder = _config.HintAttackerMessage.Replace("%TargetName%", ev.Player.Nickname).Replace("%TargetRole%", ev.Player.Role.ToString()).Replace("%Damage%", Math.Round(ev.Amount).ToString()).Replace(@"\n", Environment.NewLine);
+            string targetHintBuilder = _config.HintTargetMessage.Replace("%AttackerName%", ev.Attacker.Nickname).Replace("%AttackerRole%", ev.Attacker.Role.ToString()).Replace("%Damage%", Math.Round(ev.Amount).ToString()).Replace(@"\n", Environment.NewLine);
+            string friendlyFireBuilder = _config.FriendlyFireWarningMessage.Replace("%AttackerName%", ev.Attacker.Nickname).Replace("%AttackerRole%", ev.Attacker.Role.ToString()).Replace("%Damage%", Math.Round(ev.Amount).ToString()).Replace(@"\n", Environment.NewLine).Replace("%DamageHint%", attackerHintBuilder);
+
+            if (_config.EnableAttackerHint)
+                ev.Attacker.ShowHint(attackerHintBuilder, _config.HintAttackerDuration);
+
+            if (_config.EnableTargetHint)
+                ev.Player.ShowHint(targetHintBuilder, _config.HintTargetDuration);
+
+            if (_config.EnableFriendlyFireWarning && ev.Attacker.Role.Side == ev.Player.Role.Side)
+                ev.Attacker.ShowHint(friendlyFireBuilder, _config.FriendlyFireWarningHintDuration);
+
+            if (_config.HitMarkerSize > 1)
+                ev.Attacker.ShowHitMarker(_config.HitMarkerSize);
+        }
+        public void DyingEvent(DyingEventArgs ev)
+        {
+            if (ev.Attacker == null || ev.Player == null || ev.Attacker == ev.Player)
+                return;
+
+
+            if (_config.EnableKillHint)
+                ev.Attacker.ShowHint(_config.KillHintMessage, _config.KillHintDuration);
+        }
+    }
+}
---
+++ HitMarkers2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Exiled.API.Features;
+using Exiled.Loader.Features;
+
+using Player = Exiled.Events.Handlers.Player;
+
+namespace HitMarkers2
+{
+
+    public class HitMarkers2 : Plugin<Config>
+    {
+
+        private EventHandler EventHandler;
+        public static HitMarkers2 Singleton;
+        Config _config;
+
+        public override string Name { get; } = "HitMarkers 2";
+        public override string Author { get; } = "CapyTeam";
+        public override string Prefix { get; } = "HitMarkers2";
+        public override Version Version { get; } = new Version(2, 1, 0);
+        public override Version RequiredExiledVersion { get; } = new Version(8, 2, 1);
+
+
+        public override void OnEnabled()
+        {
+            Singleton = this;
+            EventHandler = new EventHandler();
+            _config = Config;
+
+            Player.Hurting += EventHandler.HurtingEvent;
+            Player.Dying += EventHandler.DyingEvent;
+
+            if(_config.EnableWelcomeMessage)
+                ServerConsole.AddLog($"Welcome to {WelcomeMessages.GetMessage()}", ConsoleColor.Green);
+
+            base.OnEnabled();
+        }
+
+
+        public override void OnDisabled()
+        {
+            Player.Hurting -= EventHandler.HurtingEvent;
+            Player.Dying -= EventHandler.DyingEvent;
+
+            EventHandler = null;
+            _config = null;
+            Singleton = null;
+
+            base.OnDisabled();
+        }
+    }
+}
